61935077c5c9264537f78bfc7fea63ff2696034a
John Koster <john@stillat.com>
2016-12-19 21:46:20 -0600

    null timeout

--- Magpie/Events/FinishedEvent.php
+++ Magpie/Events/FinishedEvent.php
@@ -14,6 +14,7 @@ class FinishedEvent
     {
         $process = new Process(null);
         $process->setWorkingDirectory(site_path());
+        $process->setTimeout(null);
         event('magpie.finished', $process);
     }
 
